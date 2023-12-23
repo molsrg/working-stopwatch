b9ab48a4c80efa2d8478d38e1cb15ea782316550
mlsrg <molsrg.35@gmail.com>
2023-12-23 14:05:49 +0300

    Added logic to vite.config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: `/working-stopwatch/`, 
   plugins: [
     vue({
       template: { transformAssetUrls }
